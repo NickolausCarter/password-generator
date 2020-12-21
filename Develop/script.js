eeab46ee97a26cdeb8f8f2f3fcfe711077b429ef
Pure KaoZ <nickolaus.carter81@gmail.com>
2020-12-20 19:53:51 -0600

    password criteria prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 // Assignment code here
 
-var randomPassword = {
-  length: "",
+var passwordEl = {
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   number: "0123456789",
@@ -9,6 +8,7 @@ var randomPassword = {
   value: ""
 }
 
+// random character generator functions begin
 var randomLowerCase = function () {
   var value = randomPassword.lowerCase[Math.floor(Math.random() * randomPassword.lowerCase.length)];
   console.log(value);
@@ -31,6 +31,39 @@ var randomSymbol = function () {
   var value = randomPassword.symbol[Math.floor(Math.random() * randomPassword.symbol.length)];
   console.log(value)
   return value;
+}
+// random character generator functions end
+
+var writePassword = function() {
+  var passwordLength = window.prompt("How long should your password be? Please type a number between 8 and 128.");
+
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      var lowerCaseConfirm = window.confirm("Do you want your password to contain lower case letters? 'Ok' for YES, 'Cancel' for NO.");
+      if (lowerCaseConfirm) {
+        var lowerCases = "lowercases";
+      }
+
+      var upperCaseConfirm = window.confirm("Do you want your password to contain upper case letters? 'Ok' for YES, 'Cancel' for NO.");
+      if (upperCaseConfirm) {
+        var upperCases = "uppercases";
+      }
+
+      var numberConfirm = window.confirm("Do you want your password to contain numbers? 'Ok' for YES, 'Cancel' for NO.");
+      if (numberConfirm) {
+        var numbers = "numbers";
+      }
+
+      var symbolConfirm = window.confirm("Do you want your password to contain special characters? 'Ok' for YES, 'Cancel' for NO.");
+      if (symbolConfirm) {
+        var symbols = "symbols";
+      }
+      
+      generatePassword(passwordLength, lowerCases, upperCases, numbers, symbols);
+
+    } else {
+      window.alert("Your password length is invalid, please try again.");
+      return writePassword();
+    }
 }
 
 // var generatePassword() {
