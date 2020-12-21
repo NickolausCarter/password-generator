1341b68c24b06f157ac2a647c6e0947b0dc7e26a
Pure KaoZ <nickolaus.carter81@gmail.com>
2020-12-20 22:12:36 -0600

    require at least one option to be selected

--- Develop/script.js
+++ Develop/script.js
@@ -3,35 +3,44 @@ var generateBtn = document.querySelector("#generate");
 
 // request input from user to establish parameters for password creation
 var writePassword = function() {
+  var selections = lowerCases + upperCases + numbers + symbols;
   var passwordLength = window.prompt("How long should your password be? Please type a number between 8 and 128.");
 
     if (passwordLength >= 8 && passwordLength <= 128) {
       var lowerCaseConfirm = window.confirm("Do you want your password to contain lower case letters? 'Ok' for YES, 'Cancel' for NO.");
       if (lowerCaseConfirm) {
-        var lowerCases = "lowercases";
+        var lowerCases = true;
       }
 
       var upperCaseConfirm = window.confirm("Do you want your password to contain upper case letters? 'Ok' for YES, 'Cancel' for NO.");
       if (upperCaseConfirm) {
-        var upperCases = "uppercases";
+        var upperCases = true;
       }
 
       var numberConfirm = window.confirm("Do you want your password to contain numbers? 'Ok' for YES, 'Cancel' for NO.");
       if (numberConfirm) {
-        var numbers = "numbers";
+        var numbers = true;
       }
 
       var symbolConfirm = window.confirm("Do you want your password to contain special characters? 'Ok' for YES, 'Cancel' for NO.");
       if (symbolConfirm) {
-        var symbols = "symbols";
+        var symbols = true;
       }
-      
-      generatePassword(passwordLength, lowerCases, upperCases, numbers, symbols);
+
+      // require at least one option to be selected
+      if (lowerCases || upperCases || numbers || symbols) {
+        generatePassword(passwordLength, lowerCases, upperCases, numbers, symbols);
+      } else {
+        window.alert("You must select at least one character type. Please try again.");
+        return writePassword();
+
+      }
 
     } else {
       window.alert("Your password length is invalid, please try again.");
       return writePassword();
     }
+
 }
 
 // generate password based on criteria selected in writePassword function
@@ -44,7 +53,7 @@ var generatePassword = function(passwordLength, lowerCases, upperCases, numbers,
   var passwordValue = "";
 
   if (lowerCases) {
-    passwordContainer += lowerCase;
+    passwordContainer = lowerCase;
   }
   if (upperCases) {
     passwordContainer += upperCase;
