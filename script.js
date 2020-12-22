6da329f73a7b3568543c294ec1d98e46896d9622
Pure KaoZ <nickolaus.carter81@gmail.com>
2020-12-22 12:33:05 -0600

    code refactor

--- script.js
+++ script.js
@@ -3,73 +3,60 @@ var generateBtn = document.querySelector("#generate");
 
 // request input from user to establish parameters for password creation
 var writePassword = function() {
-  var selections = lowerCases + upperCases + numbers + symbols;
-  var passwordLength = window.prompt("How long should your password be? Please type a number between 8 and 128.");
+    var passwordLength = window.prompt("How long should your password be? Please type a number between 8 and 128.");
 
     if (passwordLength >= 8 && passwordLength <= 128) {
-      var lowerCaseConfirm = window.confirm("Do you want your password to contain lower case letters? 'Ok' for YES, 'Cancel' for NO.");
-      if (lowerCaseConfirm) {
-        var lowerCases = true;
-      }
+        // user selects character types to include in password
+        var lowerCases = window.confirm("Do you want your password to contain lower case letters? 'Ok' for YES, 'Cancel' for NO.");
+        var upperCases = window.confirm("Do you want your password to contain upper case letters? 'Ok' for YES, 'Cancel' for NO.");
+        var numbers = window.confirm("Do you want your password to contain numbers? 'Ok' for YES, 'Cancel' for NO.");
+        var symbols = window.confirm("Do you want your password to contain special characters? 'Ok' for YES, 'Cancel' for NO.");
 
-      var upperCaseConfirm = window.confirm("Do you want your password to contain upper case letters? 'Ok' for YES, 'Cancel' for NO.");
-      if (upperCaseConfirm) {
-        var upperCases = true;
-      }
+        // validate at least one option was selected and pass choice(s) to generatePassword()
+        if (lowerCases || upperCases || numbers || symbols) {
+          generatePassword(passwordLength, lowerCases, upperCases, numbers, symbols);
 
-      var numberConfirm = window.confirm("Do you want your password to contain numbers? 'Ok' for YES, 'Cancel' for NO.");
-      if (numberConfirm) {
-        var numbers = true;
-      }
+        } else {
+          window.alert("You must select at least one character type. Please try again.");
+          return writePassword();
 
-      var symbolConfirm = window.confirm("Do you want your password to contain special characters? 'Ok' for YES, 'Cancel' for NO.");
-      if (symbolConfirm) {
-        var symbols = true;
-      }
-
-      // require at least one option to be selected
-      if (lowerCases || upperCases || numbers || symbols) {
-        generatePassword(passwordLength, lowerCases, upperCases, numbers, symbols);
-      } else {
-        window.alert("You must select at least one character type. Please try again.");
-        return writePassword();
-
-      }
+        }
 
     } else {
-      window.alert("Your password length is invalid, please try again.");
-      return writePassword();
+        window.alert("Your password length is invalid, please try again.");
+        return writePassword();
     }
-
 }
 
 // generate password based on criteria selected in writePassword function
 var generatePassword = function(passwordLength, lowerCases, upperCases, numbers, symbols) {
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var number = "0123456789";
-  var symbol = " !$&)'(*]+[-.#/:;<=>?@|^_`{|~";
-  var passwordContainer;
-  var passwordValue = "";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var number = "0123456789";
+    var symbol = " !$&)'(*]+[-.#/:;<=>?@|^_`{|~";
+    var passwordContainer;
+    var passwordValue = "";
 
-  if (lowerCases) {
-    passwordContainer = lowerCase;
-  }
-  if (upperCases) {
-    passwordContainer += upperCase;
-  }
-  if (numbers) {
-    passwordContainer += number;
-  }
-  if (symbols) {
-    passwordContainer += symbol;
-  }
+    // compile variable strings into container based on selections from writePassword function
+    if (lowerCases) {
+        passwordContainer = lowerCase;
+    }
+    if (upperCases) {
+        passwordContainer += upperCase;
+    }
+    if (numbers) {
+        passwordContainer += number;
+    }
+    if (symbols) {
+        passwordContainer += symbol;
+    }
 
-  for (var i = 0, x = passwordContainer.length; i < passwordLength; i++) {
-    passwordValue += passwordContainer.charAt(Math.floor(Math.random() * x));
-  }
+    // create random password with random selection from container that meets length requirement
+    for (var i = 0, x = passwordContainer.length; i < passwordLength; i++) {
+        passwordValue += passwordContainer.charAt(Math.floor(Math.random() * x));
+    }
 
-  document.querySelector("#password").innerHTML = passwordValue;
+    document.querySelector("#password").textContent = passwordValue;
 
 }
 
